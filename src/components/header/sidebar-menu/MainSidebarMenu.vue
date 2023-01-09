8c35248a5f0c51e8dade68399ae0fe93b1cc44e8
Acemint <stevenkristian300900@gmail.com>
2023-01-09 22:29:52 +0700

    add sidebar nested functionality

--- src/components/header/sidebar-menu/MainSidebarMenu.vue
+++ src/components/header/sidebar-menu/MainSidebarMenu.vue
@@ -1,76 +1,178 @@
 <template>
   <ul class="sidebar-menu">
-    <li v-for="item in dashboardItems" 
-        v-bind:class="[item.active ? 'active' : '']">
-      <router-link class="nav-link" v-bind:to="item.path">
-        <i v-bind:class="item.class"></i>
-        <span> {{ item.name }} </span>
-      </router-link>
-    </li>
+    <template v-for="item in dashboardItems">
+      <li v-if="item.dropdownProps != null"
+          v-bind:class="[item.active ? 'active show' : '', 'nav-item dropdown']">
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"> 
+          <i v-bind:class="item.logo"></i> 
+          <span> {{ item.name }} </span> 
+        </a>
+        <ul v-bind:class="[item.active? 'show' : '', 'dropdown-menu']">
+          <li v-for="childItem in item.dropdownProps" v-bind:class="[childItem.active ? 'active' : '']">
+            <router-link class="nav-link" v-bind:to="childItem.path">
+              {{ childItem.name }}
+            </router-link>
+          </li>
+        </ul>
+      </li>
+
+      <li v-else
+          v-bind:class="[item.active ? 'active' : '']">
+          <router-link class="nav-link" v-bind:to="item.path">
+            <i v-bind:class="item.logo"></i>
+            <span> {{ item.name }} </span>
+          </router-link>
+      </li>
+
+    </template>
   </ul>
 </template>
 
 <script lang="ts">
-import Path from '@/constants/path';
-import Role from '@/constants/role';
+  import Path from '@/constants/path';
+  import Role from '@/constants/role';
 
-export default {
+  export default {
 
-  data() {
-    return {
-      dashboardItems: new Array<DashboardItem>()
-    }
-  },
-
-  props: {
-    role: String
-  },
-
-  beforeMount() { 
-    this.selectDashboardItem();
-    this.updateActiveDashboardItemStyling();
-  },
-
-  methods: {
-    selectDashboardItem(): void  {
-      if (this.role === Role.CHAIRMAN) {
-        this.dashboardItems = [
-
-        ];
-      }
-      else if (this.role === Role.TREASURER) {
-        this.dashboardItems = [
-          { name: 'Dashboard', path: Path.DASHBOARD, class: 'fas fa-fire', active: false},
-          { name: 'Program Kerja', path: Path.PROGRAM, class: 'fas fa-briefcase', active: false},
-          { name: 'Laporan', path: Path.REPORT, class: 'fas fa-wallet', active: false}
-        ];
-      }
-      else if (this.role === Role.SUPERVISOR) {
-        this.dashboardItems = [
-
-        ];
+    data() {
+      return {
+        dashboardItems: new Array < DashboardItem > ()
       }
     },
-    
-    updateActiveDashboardItemStyling() {
-      for (let item of this.dashboardItems) {
-        if (item.path === this.$router.currentRoute.value.path) {
-          item.active = true;
-        } else {
-          item.active = false;
+
+    props: {
+      role: String
+    },
+
+    beforeMount() {
+      this.selectDashboardItem();
+      this.updateActiveDashboardItemStyling();
+    },
+
+    methods: {
+      selectDashboardItem(): void {
+        if (this.role === Role.SUPERVISOR) {
+          this.dashboardItems = [{
+              name: 'Dashboard',
+              path: Path.DASHBOARD,
+              logo: 'fas fa-fire',
+              active: false,
+              dropdownProps: null
+            },
+            {
+              name: 'Organisasi',
+              path: Path.ORGANIZATION,
+              logo: 'fas fa-sitemap',
+              active: false,
+              dropdownProps: null
+            },
+            {
+              name: 'Kelola Pengurus',
+              path: Path.STUDENT_MANAGEMENT,
+              logo: 'fas fa-user-alt',
+              active: false,
+              dropdownProps: null
+            },
+            {
+              name: 'Program Kerja',
+              path: Path.PROGRAM,
+              logo: 'fas fa-briefcase',
+              active: false,
+              dropdownProps: null
+            },
+            {
+              name: 'Laporan',
+              path: Path.REPORT,
+              logo: 'fas fa-file-alt',
+              active: false,
+              dropdownProps: null
+            }
+          ];
+        } else if (this.role === Role.TREASURER) {
+          this.dashboardItems = [{
+              name: 'Dashboard',
+              path: Path.DASHBOARD,
+              logo: 'fas fa-fire',
+              active: false,
+              dropdownProps: null
+            },
+            {
+              name: 'Program Kerja',
+              path: Path.PROGRAM,
+              logo: 'fas fa-briefcase',
+              active: false,
+              dropdownProps: null
+            },
+            {
+              name: 'Laporan',
+              path: Path.REPORT,
+              logo: 'fas fa-wallet',
+              active: false,
+              dropdownProps: null
+            }
+          ];
+        } else if (this.role === Role.CHAIRMAN) {
+          this.dashboardItems = [{
+              name: 'Dashboard',
+              path: Path.DASHBOARD,
+              logo: 'fas fa-fire',
+              active: false,
+              dropdownProps: null
+            },
+            {
+              name: 'Program Kerja',
+              path: Path.PROGRAM,
+              logo: 'fas fa-briefcase',
+              active: false,
+              dropdownProps: null
+            },
+            {
+              name: 'Laporan',
+              path: Path.REPORT,
+              logo: 'fas fa-wallet',
+              active: false,
+              dropdownProps: [{
+                  name: 'Laporan',
+                  path: Path.REPORT,
+                  active: false
+                },
+                {
+                  name: 'Detail Transaksi',
+                  path: Path.REPORT,
+                  active: false
+                }
+              ]
+            }
+          ];
+        }
+      },
+
+      updateActiveDashboardItemStyling() {
+        for (let item of this.dashboardItems) {
+          if (item.path === this.$router.currentRoute.value.path) {
+            item.active = true;
+          } else {
+            item.active = false;
+          }
         }
       }
+
     }
 
   }
 
-}
+  export type DashboardItem = {
+    name: string,
+    path: string,
+    logo: string,
+    active: boolean
+    dropdownProps: Array < ChildDashboardItem > | null
+  }
 
-export type DashboardItem = {
-  name: string,
-  path: string,
-  class: string,
-  active: boolean
-}
+  export type ChildDashboardItem = {
+    name: string,
+    path: string,
+    active: boolean
+  }
 
 </script>

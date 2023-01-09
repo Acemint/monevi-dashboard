c5c2c2d819441e7f1e46dfdc5a0125376126faef
Acemint <stevenkristian300900@gmail.com>
2023-01-09 20:34:04 +0700

    add error componentys

---
+++ src/views/error/Error401.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="section">
+    <div class="container mt-5">
+      <div class="page-error">
+        <div class="page-inner">
+          <h1>401</h1>
+          <div class="page-description">
+            Otorisasi diperlukan.
+          </div>
+          <div class="mt-3">
+            <router-link to="/">Back to Home</router-link>
+          </div>
+        </div>
+      </div>
+      <div class="simple-footer mt-5">
+        Copyright &copy; Monevi 2022
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>

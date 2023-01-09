d347f3c60c70b569de924d0cc24b3a5197818212
Steven Kristian <steven.kristian@gdn-commerce.com>
2023-01-09 15:52:24 +0700

    add error pages

---
+++ src/views/error/Error403.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="section">
+    <div class="container mt-5">
+      <div class="page-error">
+        <div class="page-inner">
+          <h1>403</h1>
+          <div class="page-description">
+            Anda tidak memiliki akses ke halaman ini.
+          </div>
+          <div class="mt-3">
+            <a href="index.html">Back to Home</a>
+          </div>
+        </div>
+      </div>
+      <div class="simple-footer mt-5">
+        Copyright &copy; Monevi 2022
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+  export default {
+
+  }
+</script>
---
+++ src/views/error/Error404.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="section">
+    <div class="container mt-5">
+      <div class="page-error">
+        <div class="page-inner">
+          <h1>404</h1>
+          <div class="page-description">
+            Halaman yang Anda cari tidak dapat ditemukan.
+          </div>
+          <div class="mt-3">
+            <router-link to="/">Back to Home</router-link>
+          </div>
+        </div>
+      </div>
+      <div class="simple-footer mt-5">
+        Copyright &copy; Monevi 2022
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+  export default {
+
+  }
+</script>

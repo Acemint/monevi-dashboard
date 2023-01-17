45377dde78b79b1496d81b61b4e095ce3cb89a7b
Acemint <stevenkristian300900@gmail.com>
2023-01-17 21:35:14 +0700

    feat: MON-53 - [Front-End] As a system, I can implement API for transactions

---
+++ src/views/Transaction.vue
@@ -0,0 +1,28 @@
+<template>
+  <MainHeader v-bind:role="userAccount.role" v-bind:name="userAccount.fullname" />
+  <div class="main-content">
+    <TransactionContent v-bind:organizationRegionId="userAccount.organizationRegionId" v-bind:role="userAccount.role" />
+  </div>
+  <MainFooter />
+</template>
+
+<script lang="ts">
+  import MainHeader from '@/components/header/MainHeader.vue';
+  import MainFooter from '@/components/footer/MainFooter.vue';
+  import TransactionContent from '@/components/content/TransactionContent.vue';
+  import { MoneviCookieHandler } from '@/api/methods/monevi-cookie-handler';
+
+  export default {
+    components: {
+      MainHeader,
+      TransactionContent,
+      MainFooter,
+    },
+
+    data: function () {
+      return {
+        userAccount: MoneviCookieHandler.getUserData(),
+      };
+    },
+  };
+</script>

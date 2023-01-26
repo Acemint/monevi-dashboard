74daf776dcdc591d4fb3e768c115e815ed65c17f
Vanessa Aurellia <vanessaaurellia01@gmail.com>
2023-01-26 21:57:28 +0700

    Update MainFooter.vue

--- src/components/footer/MainFooter.vue
+++ src/components/footer/MainFooter.vue
@@ -1,11 +1,10 @@
 <template>
   <footer class="main-footer">
     <div class="footer-left">
-      Copyright &copy; 2022
+      Copyright &copy; 2023
       <div class="bullet"></div>
       Monevi
     </div>
-    <div class="footer-right">1.0.0</div>
   </footer>
 </template>
 

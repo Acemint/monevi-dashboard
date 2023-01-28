4936c9c77925a3ada1efd9e0ecc8f336e33155c9
Acemint <stevenkristian300900@gmail.com>
2023-01-29 02:08:41 +0700

    get authorization move to interceptors so that it is called every time

--- src/api/configuration/monevi-axios.ts
+++ src/api/configuration/monevi-axios.ts
@@ -1,11 +1,22 @@
 import { FrontendRouteName } from '@/constants/path';
 import { router } from '@/route';
 import axios from 'axios';
+import { MoneviCookieHandler } from '../methods/monevi-cookie-handler';
 import { MONEVI_BASE_URL } from '../path/path';
 import { authorizationApi } from '../service/authorization-api';
 
 const moneviAxios = axios.create({
   baseURL: MONEVI_BASE_URL,
+});
+
+moneviAxios.interceptors.request.use(async (config) => {
+  var token = MoneviCookieHandler.getUserData();
+  if (token.accessToken != '' && token.accessToken != undefined) {
+    config.headers = {
+      Authorization: `${token.type} ${token.accessToken}`,
+    };
+  }
+  return config;
 });
 
 moneviAxios.interceptors.response.use(
@@ -20,4 +31,18 @@ moneviAxios.interceptors.response.use(
     }
   }
 );
+
+moneviAxios.interceptors.request.use((request) => {
+  // console.log('Starting Request', JSON.stringify(request, null, 2))
+  console.log('REQ', JSON.stringify(request, null, 2));
+  console.log('Starting Request Params', JSON.stringify(request.params, null, 2));
+  console.log('Starting Request Body', JSON.stringify(request.data, null, 2));
+  return request;
+});
+
+moneviAxios.interceptors.response.use((response) => {
+  console.log('Response:', JSON.stringify(response.data, null, 2));
+  return response;
+});
+
 export { moneviAxios };

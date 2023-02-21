c1730b465c410086d2f02f0e6a4d0e28f6e67489
Vanessa Aurellia <vanessaaurellia01@gmail.com>
2023-02-21 12:11:18 +0700

    asdf

--- src/components/content/IndexContent.vue
+++ src/components/content/IndexContent.vue
@@ -8,8 +8,7 @@
         <span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navcol-1">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item"><a class="nav-link active" href="/">Home</a></li>
+        <ul class="navbar-nav mx-auto navbar-right">
           <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
         </ul>
         <a class="btn btn-primary shadow" role="button" href="/register">Register</a>
@@ -219,15 +218,13 @@
                   data-bs-target="#accordion-1 .item-1"
                   aria-expanded="true"
                   aria-controls="accordion-1 .item-1">
-                  Aenean arcu euismod aliquam, volutpat consequat?
+                  Apa itu Monevi?
                 </button>
               </h2>
               <div class="accordion-collapse collapse show item-1" role="tabpanel" data-bs-parent="#accordion-1">
                 <div class="accordion-body">
                   <p>
-                    Maecenas diam volutpat, erat quis enim cras lobortis vivamus donec tempor. Congue ultrices donec
-                    turpis vivamus. Laoreet aenean metus, mi nunc massa feugiat duis. Pharetra erat consequat purus
-                    curae quisque, etiam accumsan class.
+                    Monevi adalah sebuah platform yang digunakan untuk mengatur keuangan organisasi yang berada di Universitas Bina Nusantara.
                   </p>
                   <p class="mb-0">Commodo rutrum quisque curabitur habitasse, suspendisse etiam.</p>
                 </div>
@@ -242,35 +239,14 @@
                   data-bs-target="#accordion-1 .item-2"
                   aria-expanded="false"
                   aria-controls="accordion-1 .item-2">
-                  Lorem quam erat placerat mollis, rhoncus senectus?
+                  Bagaimana cara melakukan login setelah registrasi?
                 </button>
               </h2>
               <div class="accordion-collapse collapse item-2" role="tabpanel" data-bs-parent="#accordion-1">
                 <div class="accordion-body">
                   <p class="mb-0">
-                    Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in,
-                    egestas eget quam. Donec id elit non mi porta gravida at eget metus.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="accordion-item">
-              <h2 class="accordion-header" role="tab">
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#accordion-1 .item-3"
-                  aria-expanded="false"
-                  aria-controls="accordion-1 .item-3">
-                  Iaculis accumsan id, facilisis proin ipsum velit neque?
-                </button>
-              </h2>
-              <div class="accordion-collapse collapse item-3" role="tabpanel" data-bs-parent="#accordion-1">
-                <div class="accordion-body">
-                  <p class="mb-0">
-                    Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in,
-                    egestas eget quam. Donec id elit non mi porta gravida at eget metus.
+                    Untuk melakukan login, user dengan role Bendahara atau Ketua harus menunggu pendaftarannya diterima oleh Pengawas. Pemberitahuan penerimaan akun  akan diberikan melalui email yang didaftarkan.
+Sedangkan user dengan role Pengawas hanya dapat melakukan login setelah melakukan penggantian password melalui link yang dikirimkan pada email. .
                   </p>
                 </div>
               </div>
